0df15f15529b2f3d822876aa6e1b5939df42d6d0
Nate Finch <nate.finch@gmail.com>
2016-03-15 22:56:58 -0400

    add stringmap type

---
+++ stringmap.go
@@ -0,0 +1,45 @@
+// Copyright 2012, 2013 Canonical Ltd.
+// Licensed under the LGPLv3, see LICENSE file for details.
+
+package cmd
+
+import (
+	"fmt"
+	"strings"
+)
+
+// StringMap is a type that deserializes a CLI string using gnuflag's Value
+// semantics.  It expects a name=value pair, and supports multiple copies of the
+// flag adding more pairs, though the names must be unique.
+type StringMap struct {
+	mapping *map[string]string
+}
+
+// Set implements gnuflag.Value's Set method.
+func (m StringMap) Set(s string) error {
+	if *m.mapping == nil {
+		*m.mapping = map[string]string{}
+	}
+	// make a copy so the following code is less ugly with dereferencing.
+	mapping := *m.mapping
+
+	vals := strings.SplitN(s, "=", 2)
+	if len(vals) != 2 {
+		return fmt.Errorf("badly formatted name value pair: " + s)
+	}
+	name, value := vals[0], vals[1]
+	if _, ok := mapping[name]; ok {
+		return fmt.Errorf("duplicate name specified: %q", name)
+	}
+	mapping[name] = value
+	return nil
+}
+
+// String implements gnuflag.Value's String method
+func (m StringMap) String() string {
+	pairs := make([]string, 0, len(*m.mapping))
+	for name, value := range *m.mapping {
+		pairs = append(pairs, name+"="+value)
+	}
+	return strings.Join(pairs, ";")
+}
